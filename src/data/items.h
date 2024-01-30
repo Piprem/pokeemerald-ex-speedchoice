7f41a8c280de307cc788f742fa28e208460f4bf5
Piprem <ngalik95@gmail.com>
2024-01-30 17:38:56 -0500

    fix syntax error

--- src/data/items.h
+++ src/data/items.h
@@ -975,7 +975,7 @@ const struct Item gItems[] =
     {
         .name = _("Rare Candy"),
         .itemId = ITEM_RARE_CANDY,
-        .price = 10000,
+        .price = 5,
         .description = sRareCandyDesc,
         .pocket = POCKET_ITEMS,
         .type = ITEM_USE_PARTY_MENU,
